a42bd99bef273383194ef1f8c499229a07b8b030
huang6349 <252683858@qq.com>
2026-03-21 01:28:19 +0800

    chore: 添加 Taro 编译 CSS 变量作用域注入配置
    - 在 webpack5 编译配置中启用 `injectAdditionalCssVarScope` 选项

--- config/config.ts
+++ config/config.ts
@@ -120,6 +120,7 @@ const config: UserConfigExport<'webpack5'> = {
             args: [{
               appType: 'taro',
               rem2rpx: !0,
+              injectAdditionalCssVarScope: !0,
             }],
           },
         },
