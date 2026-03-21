5c65a92cc8926f027b1ee8bc15fe191b44cbf94d
huang6349 <252683858@qq.com>
2026-03-21 16:04:44 +0800

    style: 优化 SysDrawer 动画效果

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   plugins: [],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      transitionTimingFunction: {
+        'ios': 'cubic-bezier(0.25, 0.1, 0.25, 1)',
+      },
+      transitionDuration: {
+        '250': '250ms',
+      },
+    },
+  },
   corePlugins: {
     // 小程序不需要 preflight
     preflight: process.env.TARO_ENV === 'h5'
